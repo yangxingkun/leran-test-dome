5a889449b6a33c297105d144b5c6a9dc0cbc2ab9
yangxingkun <xingkun.yang@seres.cn>
2023-05-19 15:02:56 +0800

    feat: 全局依赖提交
    了解 yarn  workspace pkg1 add或者remove packageName command  yarn workspaces run serve 执行所有包的这个命令 yarn
    -W add pageName
    
    BREAKING CHANGE: haode

--- packages/pkg1/lib/pkg1.js
+++ packages/pkg1/lib/pkg1.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = pkg1;
-
+// import _ from "lodash"
+const _ =require('lodash')
+const $ =require('jquery')
 function pkg1() {
+  console.log("pgk10000000",_)
+  console.log("pgk11111111",$)
   return 'Hello from pkg1';
 }
+pkg1()
--- packages/pkg2/lib/pkg2.js
+++ packages/pkg2/lib/pkg2.js
@@ -3,5 +3,7 @@
 module.exports = pkg2;
 
 function pkg2() {
+  console.log("pgk2789797")
   return 'Hello from pkg2';
 }
+pkg2()
