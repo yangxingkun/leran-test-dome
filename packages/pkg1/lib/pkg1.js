3a618b6d8f73df518fedd5feabc6efc5ed436596
yangxingkun <xingkun.yang@seres.cn>
2023-05-22 11:21:42 +0800

    feat(kk): kk
    kk
    
    BREAKING CHANGE: kk

--- packages/pkg1/lib/pkg1.js
+++ packages/pkg1/lib/pkg1.js
@@ -6,7 +6,7 @@ const _ =require('lodash')
 const $ =require('jquery')
 function pkg1() {
   console.log("pgk10000000",_)
-  console.log("pgk11111111",$)
+  console.log("Pkg1我叫杨兴坤",$)
   return 'Hello from pkg1';
 }
 pkg1()
--- packages/pkg2/lib/pkg2.js
+++ packages/pkg2/lib/pkg2.js
@@ -3,7 +3,7 @@
 module.exports = pkg2;
 
 function pkg2() {
-  console.log("pgk2789797")
+  console.log("pgk2杨兴坤")
   return 'Hello from pkg2';
 }
 pkg2()
